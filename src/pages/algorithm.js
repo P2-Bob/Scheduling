f97ff494ecc2ad923f40f307c96e8f401398d413
Lucas Wedel <lwedel22@student.aau.dk>
2023-04-20 12:48:16 +0200

    fuld random shift, ingen repeat medarbejder

--- src/pages/algorithm.js
+++ src/pages/algorithm.js
@@ -1,4 +1,4 @@
-const employees = ['Alice', 'Bob', 'Charlie', 'Dave', 'Eve', 'Fred', 'Grace', 'Helen', 'Irene', 'Jack', 'Kelly', 'Larry'];
+const employees = ['Alice', 'Bob', 'Charlie', 'Dave', 'Eve', 'Fred', 'Grace', 'Helen', 'Irene', 'Jack', 'Kelly', 'Larry', 'Mallory', 'Nora', 'Oscar', 'Peggy', 'Quinn', 'Robert', 'Trent', 'Uma', 'Victor', 'Wendy', 'Xavier', 'Yvonne', 'Zoe'];
 const youthemployees = ['Mia', 'Nia', 'Oia', 'Pia', 'Qia', 'Ria', 'Sia', 'Tia', 'Uia', 'Via', 'Wia', 'Xia', 'Yia', 'Zia'];
 const shifts = ['6-14', '14-22', '22-6'];
 const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
@@ -21,28 +21,36 @@ const randomEmployee = (amountOfWorkers, worker, schedule, day, shift) => {
 
         pickEmployee[amountOfWorkersCounter] = worker[Math.floor(Math.random() * worker.length)];
 
+        
+        
 
         validCount = 0
         validEmployee = false;
-        while (!validEmployee) {
+        while (!validEmployee || validCount != 0) {
+            validCount = 0
+            validEmployee = false;
+
             if (schedule[day].includes(pickEmployee[amountOfWorkersCounter])) {
                 console.log('alreadyscheduled');
             }
             validEmployee = sameEmployeeCheckShift(pickEmployee, amountOfWorkersCounter, worker);
 
-            let obj = schedule[day]
-            if (schedule.hasOwnProperty(day) && schedule[day].hasOwnProperty(shift)) {
-                const arr = Object.entries(schedule[day][shift])[0][1];
-                console.log(arr.flat());
+            if(unavailableEmployees[day].includes(pickEmployee[amountOfWorkersCounter])){
+                validCount++;
+            }
+            
+           
+
+            if (!validEmployee || validCount != 0) {
+                pickEmployee[amountOfWorkersCounter] = worker[Math.floor(Math.random() * worker.length)];
             }
 
-
-
-            sameEmployeeCheckDay(schedule, day, pickEmployee, amountOfWorkersCounter, shift, worker);
-
-
         }
+        
     }
+    unavailableEmployees[day] = unavailableEmployees[day].concat(pickEmployee);
+    
+    
 
     return pickEmployee
 }
@@ -110,8 +118,8 @@ const randomSchedule = () => {
 
             schedule[day][shift] = pickedEmployee;
 
-            unavailableEmployees[day] = unavailableEmployees[day].concat(pickedEmployee);
-            console.log("se her: ", unavailableEmployees);
+           
+            
 
         }
     }
