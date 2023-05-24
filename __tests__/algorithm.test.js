c0ea8df34530a5c719ae8cf7a175277214525503
Dressi123 <andreas.jack.2002@gmail.com>
2023-05-24 10:52:01 +0200

    fixed test

--- __tests__/algorithm.test.js
+++ __tests__/algorithm.test.js
@@ -1,24 +1,56 @@
-import generateSchedule from "/pages/algorithmscheduleimproved";
+import generateSchedule from "@/pages/algorithmscheduleimprover";
+import '@testing-library/jest-dom';
 
 const mockEmployee = [
     {username: 'emp1'},
     {username: 'emp2'},
-    {username: 'emp3'}
+    { username: 'emp3' },
+    { username: 'emp4' },
+    { username: 'emp5' },
+    { username: 'emp6' },
+    { username: 'emp7' },
+    { username: 'emp8' },
+    { username: 'emp9' },
+    { username: 'emp10' },
 ];
 
 const mockYouth = [
     {username: 'youth1'},
     {username: 'youth2'},
-    {username: 'youth3'}
+    { username: 'youth3' },
+    { username: 'youth4' },
+    { username: 'youth5' },
+    { username: 'youth6' },
+    { username: 'youth7' },
+    { username: 'youth8' },
+    { username: 'youth9' },
+    { username: 'youth10' },
 ];
 
 const preference = [
     {username: 'emp1', Monday: 1, Tuesday: 2, Wednesday: 0, Thursday: 1, Friday: 1, Saturday: 2, Sunday: 1},
-    {username: 'emp2', Monday: 2, Tuesday: 1, Wednesday: 1, Thursday: 2, Friday: 0, Saturday: 1, Sunday: 2},
-    {username: 'youth1', Monday: 1, Tuesday: 1, Wednesday: 2, Thursday: 0, Friday: 1, Saturday: 2, Sunday: 1},
+    { username: 'emp2', Monday: 2, Tuesday: 1, Wednesday: 1, Thursday: 2, Friday: 0, Saturday: 1, Sunday: 2 },
+    { username: 'emp3', Monday: 1, Tuesday: 2, Wednesday: 0, Thursday: 1, Friday: 1, Saturday: 2, Sunday: 1 },
+    { username: 'emp4', Monday: 2, Tuesday: 1, Wednesday: 1, Thursday: 2, Friday: 0, Saturday: 1, Sunday: 2 },
+    { username: 'emp5', Monday: 1, Tuesday: 2, Wednesday: 0, Thursday: 1, Friday: 1, Saturday: 2, Sunday: 1 },
+    { username: 'emp6', Monday: 2, Tuesday: 1, Wednesday: 1, Thursday: 2, Friday: 0, Saturday: 1, Sunday: 2 },
+    { username: 'emp7', Monday: 1, Tuesday: 2, Wednesday: 0, Thursday: 1, Friday: 1, Saturday: 2, Sunday: 1 },
+    { username: 'emp8', Monday: 2, Tuesday: 1, Wednesday: 1, Thursday: 2, Friday: 0, Saturday: 1, Sunday: 2 },
+    { username: 'emp9', Monday: 1, Tuesday: 2, Wednesday: 0, Thursday: 1, Friday: 1, Saturday: 2, Sunday: 1 },
+    { username: 'emp10', Monday: 2, Tuesday: 1, Wednesday: 1, Thursday: 2, Friday: 0, Saturday: 1, Sunday: 2 },
+    { username: 'youth1', Monday: 1, Tuesday: 1, Wednesday: 2, Thursday: 0, Friday: 1, Saturday: 2, Sunday: 1 },
+    { username: 'youth2', Monday: 2, Tuesday: 2, Wednesday: 1, Thursday: 1, Friday: 0, Saturday: 1, Sunday: 2 },
+    { username: 'youth3', Monday: 1, Tuesday: 1, Wednesday: 2, Thursday: 0, Friday: 1, Saturday: 2, Sunday: 1 },
+    { username: 'youth4', Monday: 2, Tuesday: 2, Wednesday: 1, Thursday: 1, Friday: 0, Saturday: 1, Sunday: 2 },
+    { username: 'youth5', Monday: 1, Tuesday: 1, Wednesday: 2, Thursday: 0, Friday: 1, Saturday: 2, Sunday: 1 },
+    { username: 'youth6', Monday: 2, Tuesday: 2, Wednesday: 1, Thursday: 1, Friday: 0, Saturday: 1, Sunday: 2 },
+    { username: 'youth7', Monday: 1, Tuesday: 1, Wednesday: 2, Thursday: 0, Friday: 1, Saturday: 2, Sunday: 1 },
+    { username: 'youth8', Monday: 2, Tuesday: 2, Wednesday: 1, Thursday: 1, Friday: 0, Saturday: 1, Sunday: 2 },
+    { username: 'youth9', Monday: 1, Tuesday: 1, Wednesday: 2, Thursday: 0, Friday: 1, Saturday: 2, Sunday: 1 },
+    { username: 'youth10', Monday: 2, Tuesday: 2, Wednesday: 1, Thursday: 1, Friday: 0, Saturday: 1, Sunday: 2 },
 ];
 
-descirbe('generateSchedule', () => {
+describe('generateSchedule', () => {
     it('should return an object which contains schedule, fitnessValue and count', async () => {
         const result = await generateSchedule(mockEmployee, mockYouth, preference);
 
